0db0e168d494bf8aa0fb56579b2e93effc449273
Nelson Chen <crazysim@gmail.com>
2019-04-27 11:48:13 -0700

    less hardcode

--- download_install_qt.ps1
+++ download_install_qt.ps1
@@ -2,7 +2,7 @@ $Wc = New-Object System.Net.WebClient ; `
 $Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'qt.exe') ; 
 Write-Output 'Downloaded QT Installer' ; `
 $Env:QT_INSTALL_DIR = Join-Path (Get-Location) "Qt"; `
-Start-Process C:\qt.exe -ArgumentList '--verbose --script windows/qtifwsilent.qs' -NoNewWindow -Wait ; `
+Start-Process qt.exe -ArgumentList '--verbose --script windows/qtifwsilent.qs' -NoNewWindow -Wait ; `
 Write-Output 'Installed QT Installer' ; `
 Remove-Item qt.exe -Force ; `
 Remove-Item Qt\MaintenanceTool.exe -Force ; `
