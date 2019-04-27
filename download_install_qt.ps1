2a6361526c0c6904d145a41ba24c1f92a56b57b6
Nelson Chen <crazysim@gmail.com>
2019-04-27 11:37:12 -0700

    --all
    first cache

--- download_install_qt.ps1
+++ download_install_qt.ps1
@@ -1,8 +1,8 @@
 $Wc = New-Object System.Net.WebClient ; `
-$Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'C:\qt.exe') ; 
+$Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'qt.exe') ; 
 Write-Output 'Downloaded QT Installer' ; `
-$Env:QT_INSTALL_DIR = 'C:\\Qt' ; `
+$Env:QT_INSTALL_DIR = Join-Path (Get-Location) "Qt"; `
 Start-Process C:\qt.exe -ArgumentList '--verbose --script windows/qtifwsilent.qs' -NoNewWindow -Wait ; `
 Write-Output 'Installed QT Installer' ; `
-Remove-Item C:\qt.exe -Force ; `
-Remove-Item C:\qt\MaintenanceTool.exe -Force ; `
+Remove-Item qt.exe -Force ; `
+Remove-Item Qt\MaintenanceTool.exe -Force ; `
