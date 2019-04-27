c57d2d263de989e68126f0c19170db3b8e54e46f
Nelson Chen <crazysim@gmail.com>
2019-04-27 15:04:11 -0700

    Don't restore on Qt existing

--- download_install_qt.ps1
+++ download_install_qt.ps1
@@ -1,8 +1,12 @@
+if (test-path Qt) {
+    Write-Host "File found!"
+    Exit
+}
 $Wc = New-Object System.Net.WebClient ; `
-$Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'qt.exe') ; 
+    $Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'qt.exe') ; 
 Write-Output 'Downloaded QT Installer' ; `
-$Env:QT_INSTALL_DIR = Join-Path (Get-Location) "Qt"; `
-Start-Process qt.exe -ArgumentList '--verbose --script windows/qtifwsilent.qs' -NoNewWindow -Wait ; `
-Write-Output 'Installed QT Installer' ; `
-Remove-Item qt.exe -Force ; `
-Remove-Item Qt\MaintenanceTool.exe -Force ; `
+    $Env:QT_INSTALL_DIR = Join-Path (Get-Location) "Qt"; `
+    Start-Process qt.exe -ArgumentList '--verbose --script windows/qtifwsilent.qs' -NoNewWindow -Wait ; `
+    Write-Output 'Installed QT Installer' ; `
+    Remove-Item qt.exe -Force ; `
+    Remove-Item Qt\MaintenanceTool.exe -Force ; `
