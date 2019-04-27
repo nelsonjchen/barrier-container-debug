e3776a4d540e2e55b02ad8a38d0caa4d285d621e
Nelson Chen <crazysim@gmail.com>
2019-04-27 10:28:53 -0700

    try installing

--- download_install_qt.ps1
+++ download_install_qt.ps1
@@ -1,8 +1,8 @@
 $Wc = New-Object System.Net.WebClient ; `
 $Wc.DownloadFile('https://download.qt.io/official_releases/qt/5.12/5.12.3/qt-opensource-windows-x86-5.12.3.exe', 'C:\qt.exe') ; 
-# Echo 'Downloaded QT Installer' ; `
-# $Env:QT_INSTALL_DIR = 'C:\\Qt' ; `
-# Start-Process C:\qt.exe -ArgumentList '--verbose --script C:/qtifwsilent.qs' -NoNewWindow -Wait ; `
-# Remove-Item C:\qt.exe -Force ; `
-# Remove-Item C:\qt\MaintenanceTool.exe -Force ; `
-# Remove-Item C:\qtifwsilent.qs -Forcef
+Write-Output 'Downloaded QT Installer' ; `
+$Env:QT_INSTALL_DIR = 'C:\\Qt' ; `
+Start-Process C:\qt.exe -ArgumentList '--verbose --script windows/qtifwsilent.qs' -NoNewWindow -Wait ; `
+Write-Output 'Installed QT Installer' ; `
+Remove-Item C:\qt.exe -Force ; `
+Remove-Item C:\qt\MaintenanceTool.exe -Force ; `
